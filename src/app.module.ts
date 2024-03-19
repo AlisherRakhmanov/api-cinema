6c5885e90401cc3f1cf5ccba48cec350d0db5238
AlisherRakhmanov <alisherrakhmanov20032@gmail.com>
2024-03-19 15:02:03 +0500

    feat: add a director model to prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ActorModule } from './actor/actor.module';
 import { AuthModule } from './auth/auth.module';
+import { GenreModule } from './genre/genre.module';
+import { MediaModule } from './media/media.module';
+import { PaginationModule } from './pagination/pagination.module';
 import { PrismaService } from './prisma.service';
 import { UserModule } from './user/user.module';
-import { PaginationModule } from './pagination/pagination.module';
-import { GenreModule } from './genre/genre.module';
-import { ActorModule } from './actor/actor.module';
 
 @Module({
-	imports: [ConfigModule.forRoot({ isGlobal: true }), UserModule, AuthModule, PaginationModule, GenreModule, ActorModule],
+	imports: [
+		ConfigModule.forRoot({ isGlobal: true }),
+		UserModule,
+		AuthModule,
+		PaginationModule,
+		GenreModule,
+		ActorModule,
+		MediaModule
+	],
 	providers: [PrismaService]
 })
 export class AppModule {}
